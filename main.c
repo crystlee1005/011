3054d3212e6262a4f4e638b11497f42f3a0addcf
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-14 20:20:39 +0900

    실습2

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
-   char *pc;
-   int *pi;
-   double *pd;
-    pc = (char *)10000;
-    pi = (int*)10000;
-    pd = (double *)10000;
-  printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-    pc++;
-    pi++;
-    pd++;
-  printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-  
+  int i = 10;
+  int *ptr = &i;
+ printf("i = %d, pi : %p\n", i, ptr);
+  (*ptr)++; 
+ printf("i = %d, pi : %p\n", i, ptr);
+  *ptr++;
+ printf("i = %d, pi : %p\n", i, ptr);
+ 
 return 0;
 }
