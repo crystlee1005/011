359e37036458bb7c835d759cdd408a1ca66890ad
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-14 20:21:44 +0900

    실습2

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void)
   int *ptr = &i;
  printf("i = %d, pi : %p\n", i, ptr);
   (*ptr)++; 
- printf("i = %d, pi : %p\n", i, ptr);
+ printf("i = %d, pi : %p\n", i, ptr); //포인터에 입력된 주소는 그대로  
   *ptr++;
  printf("i = %d, pi : %p\n", i, ptr);
  
